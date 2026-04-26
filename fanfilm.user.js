8b4bdcb27a547909c86493a3b17cba6e9818d9f6
notoco <36659110+notoco@users.noreply.github.com>
2026-04-26 10:17:26 +0200

    Update fanfilm.user.js

--- fanfilm.user.js
+++ fanfilm.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         FanFilm
 // @namespace    http://tampermonkey.net/
-// @version      0.1.20260409.0
+// @version      0.1.20260426.0
 // @description  Web service
 // @author       kpl-team
 // @match        http*://cda-hd.cc/*
 // @match        http*://zaluknij.cc/*
+// @match        http*://ekino.ws/*
+// @match        http*://ekino-tv.pl/*
 // @icon         https://raw.githubusercontent.com/fanfilm-pl/repository.fanfilm.beta/refs/heads/main/favicon.png
 // @downloadURL  https://raw.githubusercontent.com/fanfilm-pl/repository.fanfilm.beta/refs/heads/main/fanfilm.user.js
 // @updateURL    https://raw.githubusercontent.com/fanfilm-pl/repository.fanfilm.beta/refs/heads/main/fanfilm.user.js
